2a1ebbc439b8121264cd61ac5781725bab8052d1
sae <sae0728@gmail.com>
2017-02-25 13:56:57 +0900

    Added Maysonry file

---
+++ js/base.js
@@ -0,0 +1,20 @@
+//  navi menu--------------------------------------------------------
+
+jQuery(function($) {
+    $(function() {
+        var $header = $('#top-header');
+        // Nav Fixed
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 350) {
+                $header.addClass('fixed');
+            } else {
+                $header.removeClass('fixed');
+            }
+        });
+        // Nav Toggle Button
+        $('#nav-toggle').click(function(){
+            $header.toggleClass('open');
+        });
+    });
+});
+
