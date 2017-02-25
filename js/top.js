f9b5bfef2d075b215adaf21ebdc68b29717892ef
sae <sae0728@gmail.com>
2017-02-25 14:16:28 +0900

    Changed js

--- js/top.js
+++ js/top.js
@@ -1,9 +1,9 @@
 // topjs
 
-jQuery(function($){
-  $('#article').masonry({
-    itemSelector: '.item', //対象するセレクタ指定
-    columnWidth: 23.5%, //カラム幅
-    isFitWidth: true  //親要素の幅に合わせてカラム数を自動調整
-  });
+$(function(){
+$('#article').masonry({
+  itemSelector: '.item',
+  isFitWidth: true,
+  isAnimated: true
+});
 });
